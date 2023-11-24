a73c2d91ea2e6265146685cf23bd8eb0449053cd
sofyasaharani <sofyasaharani30@gmail.com>
2023-11-24 15:22:09 +0700

    membuat template tampilan video rekomendasi tahap intermediate dan expert

--- src/components/RecomendationVideos.js
+++ src/components/RecomendationVideos.js
@@ -68,6 +68,103 @@ const RecommendationVideos = ({ beginner, intermediate, expert }) => {
           </a>
         ))}
       </Stack>
+      {/* Intermediate */}
+      <Typography
+        sx={{
+          fontSize: { lg: "44px", xs: "25px" },
+          textAlign: { xs: "center", lg: "left" },
+        }}
+        fontWeight={700}
+        color="#000"
+        mt="120px"
+        mb="33px">
+        Workout for{" "}
+        <span style={{ color: "#FF2625", textTransform: "capitalize" }}>
+          intermediate
+        </span>{" "}
+      </Typography>
+      <Stack
+        sx={{
+          flexDirection: { lg: "row" },
+          gap: { lg: "20px", xs: "0px" },
+        }}
+        justifyContent="flex-start"
+        flexWrap="wrap"
+        alignItems="center"
+        marginBottom="80px">
+        {intermediate?.slice(0, 8)?.map((item, index) => (
+          <a
+            key={index}
+            className="exercise-video"
+            href={`https://www.youtube.com/watch?v=${item?.video?.videoId}`}
+            target="_blank"
+            rel="noreferrer">
+            <img
+              src={item?.video?.thumbnails[0].url}
+              alt={item?.video?.title}
+            />
+            <Box sx={{ padding: "10px" }}>
+              <Typography
+                sx={{ fontSize: { lg: "20px", xs: "18px" } }}
+                fontWeight={600}
+                color="#000">
+                {item?.video?.title}
+              </Typography>
+              <Typography fontSize="14px" color="#000">
+                {item?.video?.channelName}
+              </Typography>
+            </Box>
+          </a>
+        ))}
+      </Stack>
+      {/* Expert */}
+      <Typography
+        sx={{
+          fontSize: { lg: "44px", xs: "25px" },
+          textAlign: { xs: "center", lg: "left" },
+        }}
+        fontWeight={700}
+        color="#000"
+        mb="33px">
+        Workout for{" "}
+        <span style={{ color: "#FF2625", textTransform: "capitalize" }}>
+          expert
+        </span>{" "}
+      </Typography>
+      <Stack
+        sx={{
+          flexDirection: { lg: "row" },
+          gap: { lg: "40px", xs: "0px" },
+        }}
+        justifyContent="flex-start"
+        flexWrap="wrap"
+        alignItems="center"
+        marginBottom="80px">
+        {expert?.slice(0, 8)?.map((item, index) => (
+          <a
+            key={index}
+            className="exercise-video"
+            href={`https://www.youtube.com/watch?v=${item?.video?.videoId}`}
+            target="_blank"
+            rel="noreferrer">
+            <img
+              src={item?.video?.thumbnails[0].url}
+              alt={item?.video?.title}
+            />
+            <Box sx={{ padding: "10px" }}>
+              <Typography
+                sx={{ fontSize: { lg: "20px", xs: "18px" } }}
+                fontWeight={600}
+                color="#000">
+                {item?.video?.title}
+              </Typography>
+              <Typography fontSize="14px" color="#000">
+                {item?.video?.channelName}
+              </Typography>
+            </Box>
+          </a>
+        ))}
+      </Stack>
     </Box>
   );
 };
